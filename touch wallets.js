5f81f1295075e78b9c08e9ac6d81586ebb004ea1
Magic_Stone <158731633+SoeAung95@users.noreply.github.com>
2025-07-18 16:14:59 +0630

    Create touch wallets.js
    Signed-off-by: Magic_Stone <158731633+SoeAung95@users.noreply.github.com>

---
+++ touch wallets.js
@@ -0,0 +1,15 @@
+// 🔐 Web3 Wallets
+export const wallets = {
+  binance: {
+    eth: "0x09b5CB3dD6b741826911d094bD2364123eEfb98F",
+    bnb: "0x09b5CB3dD6b741826911d094bD2364123eEfb98F",
+    ton: "UQAKpnqJBO_ZB3NiJgsH-C-7to3hXtUYHaD-R28Vei8nw9Ut",
+    sol: "CmV5fKHWEkEDVzvuGcK12Qv8gq9XwXbe4zbjcG6LnTnc",
+  },
+  bitget: {
+    eth: "0x81635e976371EB488d5079ED46c2073F7Da2edC5",
+    bnb: "0x81635e976371EB488d5079ED46c2073F7Da2edC5",
+    sol: "9knBZoccQeyUZdPHHutNsMqe84sh7kS7iKGscqy1sTiX",
+    ton: "UQCw_Ha0_9lHiSyAoeG4CXiIJdE7J_9HWYG9RAIg7WCrIwb2",
+  },
+};
