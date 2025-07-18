811b05e41be84a721a092371a24fb7891ba86bb1
Magic-Stone <soeaung2981995@gmail.com>
2025-07-18 00:12:46 +0000

    🔥 Removed nested For-Ios folder to fix render issues

--- For-Ios/magicrepo/scripts/backup.ps1
+++
@@ -1,2 +0,0 @@
-Compress-Archive -Path .\* -DestinationPath ..\magicrepo-backup.zip -Force
-Write-Host '?? Backup created at ../magicrepo-backup.zip' -ForegroundColor Green
--- For-Ios/magicrepo/scripts/test.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host '?? Testing Grass Miner...'
-Start-Sleep -Milliseconds 500
-Write-Host '? Grass Miner is responding!'
-
-Write-Host '?? Testing GPT CLI...'
-Start-Sleep -Milliseconds 500
-Write-Host '? GPT CLI is ready!'
