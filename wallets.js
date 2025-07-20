6c9d22cdce071244fb32bb95035253da61a40b11
Magic_Stone <158731633+SoeAung95@users.noreply.github.com>
2025-07-21 01:01:52 +0630

    Update wallets.js
    Signed-off-by: Magic_Stone <158731633+SoeAung95@users.noreply.github.com>

---
+++ wallets.js
@@ -0,0 +1,33 @@
+// 📦 Magic’s Web3 Wallets Data
+const wallets = [
+  {
+    name: "💰 Ethereum (ETH)",
+    address: "0x1396b538371366cc47ac0c5670bc6883588fa101",
+    image: "https://imagedelivery.net/u8GMYyd0Bs8kckF-WHWZcg/eth/thumbnail",
+    type: "eth"
+  },
+  {
+    name: "🟡 Binance Smart Chain (BNB)",
+    address: "0x09b5CB3dD6b741826911d094bD2364123eEfb98F",
+    image: "https://imagedelivery.net/u8GMYyd0Bs8kckF-WHWZcg/bnb/thumbnail",
+    type: "bnb"
+  },
+  {
+    name: "🪙 TON Wallet",
+    address: "UQAKpnqJBO_ZB3NiJgsH-C-7to3hXtUYHaD-R28Vei8nw9Ut",
+    image: "https://imagedelivery.net/u8GMYyd0Bs8kckF-WHWZcg/ton/thumbnail",
+    type: "ton"
+  },
+  {
+    name: "🌞 Phantom Wallet (SOL)",
+    address: "7p9E6n6ab2ctZLUdYvdCqFZEQ5YmMxoNgf2cnBcvZN7U",
+    image: "https://imagedelivery.net/u8GMYyd0Bs8kckF-WHWZcg/solana/thumbnail",
+    type: "sol"
+  },
+  {
+    name: "🕰️ Legacy Trust Wallet (ETH/BSC)",
+    address: "0x678Aa08AB707CBa6d8eda67742358b8006F81829",
+    image: "https://imagedelivery.net/u8GMYyd0Bs8kckF-WHWZcg/legacy/thumbnail",
+    type: "legacy"
+  }
+];
