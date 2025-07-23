286bc1456274f76005da128122c23e73d1b2422a
Magic_Stone <158731633+SoeAung95@users.noreply.github.com>
2025-07-23 09:43:57 +0630

    Create .env.js
    Signed-off-by: Magic_Stone <158731633+SoeAung95@users.noreply.github.com>

---
+++ .env.js
@@ -0,0 +1,7 @@
+// .env.js
+// This is your local OpenAI API key.
+// DO NOT commit this file to public repositories.
+window.OPENAI_API_KEY = "sk-proj-t
+rmD5ZzL2YPvzhWPNQAsWAgBPKPcJaEKK7h9WozL8SFdqOMFLgmrW_uP
+8_qdMxgjeFnQtKH5geT3BlbkFJQvfYifWAUctHQxe9S11NaULNu19tr
+EKqRRYWwH5PUE8RE_WPWWmqZz6CWhk1xDV11fVekiJy8A";
