c2a41fb565bf3723172c58b635abc7f1ca435aa3
Magic_Stone <158731633+SoeAung95@users.noreply.github.com>
2025-07-23 10:23:46 +0630

    Update service-worker.js
    Signed-off-by: Magic_Stone <158731633+SoeAung95@users.noreply.github.com>

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,17 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("magic-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/wallet.js"]);
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/wallet.js",
+        "/wallets.js",
+        "/.env.js",
+        "/manifest.json",
+        "/apple-touch-icon.png",
+        "/icon-512x512.png"
+      ]);
     })
   );
 });
