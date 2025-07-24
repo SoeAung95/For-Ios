45ab01c87c562ec61a00759e362679dac9ebdffa
Magic_Stone <158731633+SoeAung95@users.noreply.github.com>
2025-07-24 18:21:57 +0630

    Update .env.js
    Signed-off-by: Magic_Stone <158731633+SoeAung95@users.noreply.github.com>

--- .env.js
+++ .env.js
@@ -4,4 +4,8 @@
 window.OPENAI_API_KEY = "sk-proj-t
 rmD5ZzL2YPvzhWPNQAsWAgBPKPcJaEKK7h9WozL8SFdqOMFLgmrW_uP
 8_qdMxgjeFnQtKH5geT3BlbkFJQvfYifWAUctHQxe9S11NaULNu19tr
-EKqRRYWwH5PUE8RE_WPWWmqZz6CWhk1xDV11fVekiJy8A"; // Replace with your actual API key
+EKqRRYWwH5PUE8RE_WPWWmqZz6CWhk1xDV11fVekiJy8A", // OpenAI API Key
+OPENAI_API_ENDPOINT: "https://api.openai.com/v1/chat/completions",
+  MODEL: "gpt-4o",
+  WALLET_REQUIRED: true, // true = Web3 auth only
+};
