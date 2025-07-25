2d8dee54c37cfaa8903888f39f32e67ee44e3625
SoeAung95 <krystalrosy.magic@gmail.com>
2025-07-25 01:22:19 +0000

    🚫 Fix .gitignore: untrack .env.js and git internals

--- .env.js
+++
@@ -1,3 +0,0 @@
-window.env = {
-  OPENAI_API_KEY: "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"
-};
